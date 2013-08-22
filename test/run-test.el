3fef3291f97b685162b8e0861b3f93b3ef5196ad
pogin503 <pogin503@gmail.com>
2013-08-23 08:23:18 +0900

    Update header and function name

--- test/run-test.el
+++ test/run-test.el
@@ -1,50 +1,47 @@
-;; Usage:
-;;
-;; emacs -Q -l tests/run-test.el # interactive mode
-;; emacs -batch -Q -l tests/run-test.el # batch mode
+;;; run-test --- run-test
+;;; Commentary:
 
-;; @see https://github.com/uk-ar/key-combo/blob/master/test/run-test.el
+;;; Usage:
+;;    emacs -Q -l tests/run-test.el # interactive mode
+;;    emacs -batch -Q -l tests/run-test.el # batch mode
+;;  @see https://github.com/uk-ar/key-combo/blob/master/test/run-test.el
+;;  @see http://d.hatena.ne.jp/yuheiomori0718/20111230/1325246364
+;;  @see http://sakito.jp/emacs/emacsshell.html#path
+
+;;; Code:
+
+(message "Running tests on Emacs %s" emacs-version)
 
 ;; Utils
-(defun r-like-example-test-join-path (path &rest rest)
+(defun ex-test-join-path (path &rest rest)
   "Join a list of PATHS with appropriate separator (such as /).
 
 \(fn &rest paths)"
   (if rest
-      (concat (file-name-as-directory path) (apply 'r-like-example-test-join-path rest))
+      (concat (file-name-as-directory path) (apply 'ex-test-join-path rest))
     path))
 
-(defvar r-like-example-test-dir (file-name-directory load-file-name))
-(defvar r-like-example-root-dir (concat r-like-example-test-dir ".."))
+(defconst ex-test-dir
+  (if load-file-name
+      (file-name-directory load-file-name)
+    ;; Fall back to default directory (in case of M-x eval-buffer)
+    default-directory)
+  "Directory of the test suite.")
 
+(defconst ex-root-dir (expand-file-name (concat ex-test-dir "..")))
 
 ;; Setup `load-path'
 (mapc (lambda (p) (add-to-list 'load-path p))
-      (list r-like-example-test-dir
-            r-like-example-root-dir))
+      (list ex-test-dir
+            ex-root-dir))
 
-
-;; Use ERT from github when this Emacs does not have it
-(unless (locate-library "ert")
-  (add-to-list
-   'load-path
-   (r-like-example-test-join-path r-like-example-root-dir "ert")))
-
-;; ;; Use el-spec
-;; (add-to-list
-;;  'load-path
-;;  (r-like-example-test-join-path r-like-example-root-dir "lib" "el-spec"))
-
-;; ;; el-mock
-;; (add-to-list
-;;  'load-path
-;;  (r-like-example-test-join-path r-like-example-root-dir "lib" "el-mock"))
-
-;; Load tests
 (load "r-like-example-test")
 
+;; (require 'cask "~/.cask/cask.el")
+;; (cask-initialize)
 
 ;; Run tests
-(if noninteractive
-    (ert-run-tests-batch-and-exit)
-  (ert t))
+(when noninteractive
+    (ert-run-tests-batch-and-exit))
+
+;;; run-test ends here
