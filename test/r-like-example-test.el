ac204ddda353f18b8dfc3e2a3d36edcbacbe3a64
pogin503 <pogin503@gmail.com>
2014-08-27 11:54:44 +0900

    Update tests

--- test/r-like-example-test.el
+++ test/r-like-example-test.el
@@ -1,21 +1,22 @@
-;;; r-like-example-test --- r-like-example-test
+;;; r-like-example-test.el --- r-like-example-test
 ;;; Commentary:
 ;;; Code:
 (require 'ert)
 (require 'r-like-example)
 (require 'elisp-examples)
 
-;; (add-to-list 'popwin:special-display-config '("*example*" :position right :width 50))
+;; (add-to-list 'popwin:special-display-config '("*example*" :position right :width 50 :stick t))
 
 (defun ex-test-set-env ()
   "Set r-like-example env."
-  (defconst test-example-foo '("(message \"t\" ))"))
+  (defconst test-example-foo '("(message \"t\")"))
 
   (defconst test-example-bar '("(defun __ex-bar (bool) (if bool t nil))"
                            "(__ex-bar t)"
                            "(__ex-bar nil)"))
 
   (ex-put-example 'ex-foo test-example-foo)
+  (ex-put-example 'ex-bar test-example-bar)
   )
 
 (ex-test-set-env)
@@ -23,32 +24,26 @@
 (ert-deftest addition-test ()
   (should (= (+ 1 2) 3)))
 
-(ert-deftest ex-put-example-test ()
-  (ex-test-set-env)
-  (message (car test-example-foo))
-  (should (equal '("(message \"t\" ))") (ex-put-example 'ex-foo test-example-foo))))
-
 (ert-deftest ex-get-example-test ()
-  (ex-test-set-env)
-  (should (equal test-example-foo (ex-get-example 'ex-foo))))
+  (should (equal '("(message \"t\")") (ex-get-example 'ex-foo))))
+
+(ert-deftest ex-put-example-test ()
+  (should (equal '("(message \"t\")") (ex-put-example 'ex-foo test-example-foo))))
+
 (ert-deftest ex-key-exists-p-test ()
   (should (equal t (ex-key-exists-p 'setq t)))
   (should (equal "setq: exist" (ex-key-exists-p 'setq))))
 
 (ert-deftest ex-example-test ()
-  (should (equal (mapconcat 'identity `(,(car test-example-foo) ";=> \"t\""  ,ex-separator) "\n")
+  (should (equal (mapconcat 'identity
+                            (list (car test-example-foo)
+                                  (concat ex-begin-comment "\"t\"")
+                                  ex-separator)
+                            "\n")
                  (progn
-                   (let (buf (current-buffer))
-                     (save-excursion
-                       (get-buffer-create ex-buffer-name)
-                       (switch-to-buffer ex-buffer-name)
-                       (erase-buffer)
-                       (switch-to-buffer buf)
-                       (ex-example 'ex-foo)
-                       (switch-to-buffer ex-buffer-name)
-                       (setq res (buffer-substring-no-properties (point-min) (point-max)))
-                       (switch-to-buffer buf))
-                     res)))))
+                   (ex-example 'ex-foo)
+                   (buffer-string)
+                   ))))
 
 
 ;; ;; dump example
@@ -56,4 +51,4 @@
 ;;              (ex-example (intern-soft key)))
 ;;          ex-hash)
 
-;;; r-like-example-test ends here
+;;; r-like-example-test.el ends here
